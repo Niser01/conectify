39f821bdae4edbf54e91ccda93a6bb498a1b28b2
Sergio Nicolás <shaldaneg@unal.edu.co>
2023-03-10 14:15:06 -0500

    Update repository.go

--- swarch_conectify/conectify_users_ms/internal/repository/repository.go
+++ swarch_conectify/conectify_users_ms/internal/repository/repository.go
@@ -3,13 +3,18 @@ package repository
 import "context"
 
 type Repository interface {
-	createUser(ctx context.Context, names string, lastNames string, eMail string, status int, phoneNumber float32) error
-	getuserByid(ctx context.Context, id int) (string, string, string, int, float32, error)
-	getuserByemail(ctx context.Context, eMail string) (int, string, string, string, int, float32, error)
-	getuserByname(ctx context.Context, names string) (int, string, string, string, int, float32, error)
-	getuserBylastname(ctx context.Context, lastNames string) (int, string, string, string, int, float32, error)
-	getuserBypnumber(ctx context.Context, phoneNumber float32) (int, string, string, string, int, float32, error)
-	edituserByid(ctx context.Context, id int, names string, lastNames string, eMail string, status int, phoneNumber float32) error
-	deleteuserByid(ctx context.Context, id int) error
-	editstatusByid(ctx context.Context, id int, status int) error
+	create_user(ctx context.Context, names string, lastNames string, eMail string, status int, phoneNumber float32) error
+	read_userByid(ctx context.Context, id int) (string, string, string, int, float32, error)
+	read_userByemail(ctx context.Context, eMail string) (int, string, string, string, int, float32, error)
+	read_userByname(ctx context.Context, names string) (int, string, string, string, int, float32, error)
+	read_userBylastname(ctx context.Context, lastNames string) (int, string, string, string, int, float32, error)
+	read_userBypnumber(ctx context.Context, phoneNumber float32) (int, string, string, string, int, float32, error)
+	update_userByid(ctx context.Context, id int, names string, lastNames string, eMail string, status int, phoneNumber float32) error
+	delete_userByid(ctx context.Context, id int) error
+	edit_statusByid(ctx context.Context, id int, status int) error
+
+	create_savedElement(ctx context.Context, idUser int, idElement int, idType int) error
+	read_savedElements(ctx context.Context, idUser int, idElement int) (int, error)
+	delete_savedElement(ctx context.Context, idElement int) error
+	delete_allsavedElements(ctx context.Context, idUser int) error
 }
