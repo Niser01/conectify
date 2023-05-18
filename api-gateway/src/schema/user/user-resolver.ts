4c8369ef51b1762361de1c4b850a995dec68583a
Sergio Nicolás <shaldaneg@unal.edu.co>
2023-05-18 10:04:52 -0500

    Updated cnst URL

--- api-gateway/src/schema/user/user-resolver.ts
+++ api-gateway/src/schema/user/user-resolver.ts
@@ -3,8 +3,7 @@ import axios from "axios";
 import { User, SavedElement } from "./user-type.js";
 import { url, port } from './user_server.js';
 
-const URL =  "http://localhost:8080";
-//process.env.USERS_URL ||
+const URL = process.env.USERS_URL || "http://localhost:8080";
 
 @Resolver(User)
 export default class UserResolver {
